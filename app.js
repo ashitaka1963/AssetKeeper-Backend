134752dd8fd18e9cfdecd3dc3f5e1150747c55e5
ashitaka1963 <ashitaka1963@gmail.com>
2023-08-06 08:43:25 +0900

    feat: MongoDB初期セットアップ

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./openapi.json");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
+
+// MongoDBに接続
+mongoose
+  .connect("mongodb://127.0.0.1:27017/AssetKeeper", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 // Swagger UIの設定
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
